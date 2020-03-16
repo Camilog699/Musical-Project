a3723d98348a709070c245ec815795a35216cf2c
Camilog699 <amraga10@gmail.com>
2020-03-16 11:35:52 -0500

    draw

--- GUI/GUI.py
+++ GUI/GUI.py
@@ -3,6 +3,7 @@ import sys
 import subprocess
 import os
 import ctypes
+from time import sleep
 pygame.init()
 
 
@@ -33,27 +34,32 @@ class GUI:
                     pygame.quit()
                     sys.exit()
             self.drawAuto(screen)
+            sleep(2.0)
+            self.generateChange(screen)
             pygame.display.update()
 
     def drawAuto(self, screen):
-        x = 0
-        y = 0
         for i in range(len(self.automaton.cells)):
             for j in range(len(self.automaton.cells[i])):
                 # conditions to assignment of colors
-                self.assignColors(self.automaton.cells[i][j], self.automaton.cellsT[i][j], self.automaton.cellsTh[i][j], screen)
-            y = y + 40
-            x = 0
+                self.assignColors(
+                    self.automaton.cells[i][j], self.automaton.cellsT[i][j], self.automaton.cellsTh[i][j], screen)
 
     def assignColors(self, cell, cellT, cellTh, screen):
         white = (255, 255, 255)
         black = (0, 0, 0)
 
         pygame.draw.rect(screen, black, ((cell.posX, cell.posY, 35, 35)), 1)
-        pygame.draw.rect(screen, white, ((cell.posX+2.9, cell.posY+2.9, 30, 30)))
+        pygame.draw.rect(
+            screen, white, ((cell.posX+2.9, cell.posY+2.9, 30, 30)))
 
         pygame.draw.rect(screen, black, ((cellT.posX, cellT.posY, 35, 35)), 1)
-        pygame.draw.rect(screen, white, ((cellT.posX+2.9, cell.posY+2.9, 30, 30)))
+        pygame.draw.rect(
+            screen, white, ((cellT.posX+2.9, cell.posY+2.9, 30, 30)))
 
-        pygame.draw.rect(screen, black, ((cellTh.posX, cellTh.posY, 35, 35)), 1)
-        pygame.draw.rect(screen, white, ((cellTh.posX+2.9, cellTh.posY+2.9, 30, 30)))
+        pygame.draw.rect(
+            screen, black, ((cellTh.posX, cellTh.posY, 35, 35)), 1)
+        pygame.draw.rect(
+            screen, white, ((cellTh.posX+2.9, cellTh.posY+2.9, 30, 30)))
+
+    def generateChange(screen):
